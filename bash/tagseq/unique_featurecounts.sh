5a2cc640608b2072fec27b19fc1ee2aac08bf741
michaeltconnelly <mconnelly@rsmas.miami.edu>
2022-08-23 13:14:01 -0400

    changes to featureCounts for hydra

--- bash/tagseq/unique_featurecounts.sh
+++ bash/tagseq/unique_featurecounts.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 #./bash/tagseq/unique_featurecounts.sh
 #purpose: mark duplicate reads and quantify STAR-aligned BAM files at the gene level using featureCounts
-#To start this job from the anti_phys directory, use:
-#bsub -P transcriptomics < ./bash/tagseq/unique_featurecounts.sh
-
-#BSUB -J feat
-#BSUB -q general
-#BSUB -P transcriptomics
-#BSUB -o unique_featurecounts%J.out
-#BSUB -e unique_featurecounts%J.err
-#BSUB -n 8
-#BSUB -u mconnelly@rsmas.miami.edu
-#BSUB -N
 
 #specify variable containing sequence file prefixes and directory paths
-mcs="/scratch/projects/transcriptomics/mikeconnelly"
-prodir="/scratch/projects/transcriptomics/mikeconnelly/projects/anti_phys"
+mcs="/scratch/nmnh_corals/connellym"
+prodir="/scratch/nmnh_corals/connellym/projects/anti_phys"
 
 # making a list of sample names
-samples=$(cat ${prodir}/data/quantseq_samples.txt)
+files=$(ls /scratch/nmnh_corals/connellym/projects/anti_phys/data/quantseq_reads/)
+samples=$(echo "$files" | cut -d . -f 1 | sort -u)
 
-module load samtools
-module load subread
+module load bioinformatics/samtools
 
-#for samp in $samples ; do
-#samtools index ${prodir}/outputs/alignments/${samp}_Aligned.sortedByCoord.out.bam
-#samtools view -q 255 -Sub ${prodir}/outputs/STARalign_Pdam/${samp}_PdamAligned.sortedByCoord.out.bam \
-#-o ${prodir}/outputs/STARalign_Pdam/${samp}_Aligned.sortedByCoord.out.uniq.bam
-#done
+for sample in $samples ; do
+samtools index ${prodir}/outputs/alignments/${sample}_Aligned.sortedByCoord.out.bam
+samtools view -q 255 -Sub ${prodir}/outputs/STARalign_Pdam/${samp}_PdamAligned.sortedByCoord.out.bam \
+-o ${prodir}/outputs/STARalign_Pdam/${sample}_Aligned.sortedByCoord.out.uniq.bam
+done
 
-featureCounts -p -T 8 -t gene \
+${mcs}/programs/subread-2.0.3-source/bin/featureCounts -p -T 8 -t gene \
 -g ID \
--a ${mcs}/sequences/genomes/coral/pocillopora/pdam_genome.gff \
+-a /home/connellym/sequences/pdam_annotation.gff3 \
 -o ${prodir}/outputs/STARcounts_Pdam/PocAnti_Pdam.counts \
 ${prodir}/outputs/STARalign_Pdam/*uniq.bam
