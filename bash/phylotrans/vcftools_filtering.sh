361b9bfa0bc8031daa79deb2c62f8d5b0b980da4
michaeltconnelly <mconnelly@rsmas.miami.edu>
2022-08-24 09:08:06 -0400

    add vcf filtering script

---
+++ bash/phylotrans/vcftools_filtering.sh
@@ -0,0 +1,36 @@
+# /bin/sh
+# ----------------Parameters---------------------- #
+#$  -S /bin/sh
+#$ -q sThC.q
+#$ -l mres=4G,h_data=4G,h_vmem=4G
+#$ -cwd
+#$ -j y
+#$ -N vcftools_filter_primary
+#$ -o /scratch/nmnh_corals/connellym/projects/anti_phys/bash/jobs/vcftools_filter_primary.log
+#$ -m bea
+#$ -M connellym@si.edu
+#
+# ----------------Modules------------------------- #
+module load bioinformatics/vcflib
+module load bioinformatics/vcftools
+#
+# ----------------Your Commands------------------- #
+#
+echo + `date` job $JOB_NAME started in $QUEUE with jobID=$JOB_ID on $HOSTNAME
+#
+VCF_NAME="$1"
+prodir="/scratch/nmnh_corals/connellym/projects/anti_phys"
+VCF_IN="${prodir}/outputs/phylotrans_Pdam/${VCF_NAME}.vcf.gz"
+VCF_OUT="${prodir}/outputs/phylotrans_Pdam/${VCF_NAME}_filtered_primary.vcf.gz"
+MAF="0.01"
+MISS="0.15"
+QUAL="30"
+MIN_DEPTH="20"
+MAX_DEPTH="300"
+vcftools --gzvcf $VCF_IN \
+--remove-indels --maf $MAF --max-missing $MISS --minQ $QUAL \
+--min-meanDP $MIN_DEPTH --max-meanDP $MAX_DEPTH \
+--minDP $MIN_DEPTH --maxDP $MAX_DEPTH --recode --stdout | gzip -c > \
+$VCF_OUT
+#
+echo = `date` job $JOB_NAME done
